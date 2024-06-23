da827ede68f6a0e6ed7b7c84d5c9b86255eabe56
chenglinz <onepiece8971@163.com>
2024-06-24 02:06:06 +0800

    up

--- src/shape/Grid.ts
+++ src/shape/Grid.ts
@@ -6,7 +6,7 @@ export default class Grid extends Shape {
   public row = 1
   public col = 1
   public selected: number[] = []
-  public selectedFillStyle: string
+  public selectedFillStyle: string = 'rgba(0, 0, 255, 0.1)'
 
   constructor(item: any, index: number) {
     super(item, index)
